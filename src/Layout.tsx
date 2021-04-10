2f2a01bb510dd058165dbd411b9b54705b2662d7
thekauer <thekauer@gmail.com>
2021-04-11 01:57:04 +0200

    added konami

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,9 +4,15 @@ import Footer from './components/Footer/Footer';
 import ScrollTop from './components/ScrollTop/ScrollTop';
 import Toolbar from './components/Toolbar/Toolbar';
 import './Layout.css'
+import useKonami from 'react-use-konami';
 
 export default function Layout({children}:any) {
     const [checked,setChecked] = useState(false);
+    useKonami(() => {
+        console.log('hi easter egg!');
+    }, {
+        code: ['r','o','c','k','e','f','e','l','l','e','r'], 
+    });
     return (
             <>
             <BrowserRouter>
