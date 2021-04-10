ab7eeec90875807e7f913edabce56f130d32e4eb
thekauer <thekauer@gmail.com>
2021-04-10 22:28:54 +0200

    fixed login button

--- src/components/LoginButton/LoginButton.tsx
+++ src/components/LoginButton/LoginButton.tsx
@@ -1,19 +1,18 @@
 import '../../index.css';
 import './LoginButton.css'
 import React, { useEffect, useState } from 'react';
-import usrIcon from './images/usr.svg';
 import settingsIcon from './images/settings.png'
-import {  doGoogleSignIn, signOut,auth } from '../../firebase';
+import { doGoogleSignIn, signOut, auth } from '../../firebase';
 
 const LoginButton = () => {
-    const [userProfile, setUserProfile]   = useState(usrIcon.toString());
+    const [userProfile, setUserProfile] = useState<string|null>(null);
     const [loggedIn, setLoggedIn] = useState(false);
     const [loggedInAsAdmin, setLoggedInAsAdmin] = useState(false);
 
     const authUser = () => {
         if (!loggedIn) {
             doGoogleSignIn().then((cred) => {
-                    if(cred.user!=null && cred.user.photoURL) {
+                if (cred.user != null && cred.user.photoURL) {
                     setUserProfile(cred.user.photoURL);
                     setLoggedIn(true);
                     if (cred.user.email === "thekauer@gmail.com") {
@@ -23,7 +22,7 @@ const LoginButton = () => {
             })
         } else {
             signOut().then(() => {
-                setUserProfile(usrIcon);
+                setUserProfile(null);
                 setLoggedIn(false);
                 setLoggedInAsAdmin(false);
             })
@@ -35,9 +34,18 @@ const LoginButton = () => {
 
     return (
         <>
-            {loggedInAsAdmin && <img src={settingsIcon} className="usr-icon leftmost" alt="gear" onClick={toAdminPage}></img>}
-
-            <img src={userProfile} alt="" className="usr-icon" onClick={authUser}/>
+            <div className="loginbtn">
+            {loggedInAsAdmin && <img src={settingsIcon} className="usr-icon" alt="gear" onClick={toAdminPage}></img>}
+            <div className="profile" onClick={authUser}>
+            {userProfile ? <img src={userProfile} alt="Profile" className="usr-icon"/> :
+            <svg width="16px" height="16px" className="usr-icon" fill="none" viewBox="0 0 600 600" strokeWidth="30px">
+                <circle cx="300" cy="300" r="265" />
+                <circle cx="300" cy="230" r="115"/>
+                <path d="M106.81863443903,481.4 a205,205 1 0,1 386.36273112194,0" stroke-linecap="butt" />
+            </svg>
+            }
+            </div>
+            </div>
         </>
     );
 }
