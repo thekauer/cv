568a5e009e1e58051f209645060c69a95fd16da7
thekauer <thekauer@gmail.com>
2021-04-10 21:03:11 +0200

    updated homepage

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,9 +1,9 @@
 import '../../index.css'
 import './Home.css'
-import { Skill, SkillContainer } from '../../components/Skill/Skill'
+import { Skill } from '../../components/Skill/Skill'
 import React from 'react';
 import PortfolioCard from '../../components/PortfolioCard/PortfolioCard';
-import { appDesc, appName, fsDesc, fsName, phdDesc, phdName, tdkDesc, tdkName } from '../../content';
+import { appDesc, appName, cvDesc, cvName, fsDesc, fsName, phdDesc, phdName, tdkDesc, tdkName } from '../../content';
 const Home = () => {
     return (
         <article className="home">
@@ -16,25 +16,28 @@ const Home = () => {
             </svg>
             <section id="portfolio">
                 <div className="portfolio-container">
-                    <PortfolioCard title={fsName} desc={fsDesc} color="var(--blue)" icon="" path="/fusion"/>
+                    <PortfolioCard title={fsName} desc={fsDesc} color="var(--blue)" icon="" path="/fusion" />
                     <PortfolioCard title={phdName} desc={phdDesc} color="var(--yellow)" icon="" path="/phd" />
-                    <PortfolioCard title={appName} desc={appDesc} color="rgb(76 203 45)" icon="" path=""/>
-                    <PortfolioCard title={tdkName} desc={tdkDesc} color="rgb(211 2 2)" icon="" path=""/>
+                    <PortfolioCard title={cvName} desc={cvDesc} color="var(--react)" icon="" path="" />
+                    <PortfolioCard title={appName} desc={appDesc} color="rgb(76 203 45)" icon="" path="" />
+                    <PortfolioCard title={tdkName} desc={tdkDesc} color="rgb(211 2 2)" icon="" path="" />
                 </div>
                 <svg viewBox='0 0 100 100' preserveAspectRatio='none'>
                     <path d='m 0,100 h 100 l 0,-90z' id='bottom' />
                 </svg>
             </section>
 
-
-            <SkillContainer>
-                <Skill title={"Linux"}>
-                    A laptopomon 3 évig linux volt, mert csak az ment el rajta. Annyira rossz volt a helyzet, hogy amikor megpróbáltam gnome-os Manjaro-t rakni rá, attól, hogy betöltött az UI <strong>lefagyott</strong> a laptop. Szóval úgy használtam, hogy direkt nem indítottam el az x-server-t, és gyakorlatilag <strong>command line</strong>-ban vezéreltem a rendszert. Ezáltal értékes <strong>linuxos</strong> tapasztalatra tettem szert.
-        </Skill>
-                <Skill title={"Soft skillek"}>
-                    Már több mint 7 éve tartok karate edzéseket, nagyrészt fiataloknak. Ehhez minden létező soft skillre szükségem volt. Többek között <strong>csapatmunkára</strong>, sokszor ketté osztjuk a csapatot és én kapom a hajtépős felét. Illetve kiváló <strong>kommunikációs képessé</strong>gre és <strong>konfliktus kezelésre</strong>. Mert amikor meglátom, hogy a nagyobbik gyerek akkora pofont ad a kissebbiknek, hogy a padló adja a párját, olyankor mondom neki, hogy akkor edzés végéig velem lesz párban.
-        </Skill>
-            </SkillContainer>
+            <section className="skills">
+                <header><h2>Skillek</h2></header>
+                <div className="skill-container">
+                    <Skill title={"Linux"}>
+                        A laptopomon 3 évig linux volt, mert csak az ment el rajta. Annyira rossz volt a helyzet, hogy amikor megpróbáltam gnome-os Manjaro-t rakni rá, attól, hogy betöltött az UI <strong>lefagyott</strong> a laptop. Szóval úgy használtam, hogy direkt nem indítottam el az x-server-t, és gyakorlatilag <strong>command line</strong>-ban vezéreltem a rendszert. Ezáltal értékes <strong>linuxos</strong> tapasztalatra tettem szert.
+                </Skill>
+                    <Skill title={"Soft skillek"}>
+                        Már több mint 7 éve tartok karate edzéseket, nagyrészt fiataloknak. Ehhez minden létező soft skillre szükségem volt. Többek között <strong>csapatmunkára</strong>, sokszor ketté osztjuk a csapatot és én kapom a hajtépős felét. Illetve kiváló <strong>kommunikációs képessé</strong>gre és <strong>konfliktus kezelésre</strong>. Mert amikor meglátom, hogy a nagyobbik gyerek akkora pofont ad a kissebbiknek, hogy a padló adja a párját, olyankor mondom neki, hogy akkor edzés végéig velem lesz párban.
+                </Skill>
+                </div>
+            </section>
         </article>
     );
 }
