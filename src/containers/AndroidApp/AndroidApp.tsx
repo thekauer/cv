60f1ef6475eefd859e9651e510fcd9de702a9987
thekauer <thekauer@gmail.com>
2021-04-11 17:46:53 +0200

    added android styles

--- src/containers/AndroidApp/AndroidApp.tsx
+++ src/containers/AndroidApp/AndroidApp.tsx
@@ -1,9 +1,34 @@
-import '../../index.css'
-
+import { Helmet } from 'react-helmet';
+import { appDesc, appName } from '../../content';
+import '../../index.css';
+import './AndroidApp.css';
 export const AndroidApp = () => {
     return (
         <>
-        <h1>Android app</h1>
+            <Helmet>
+                <title>Android Alkalmazás - Kauer András</title>
+            </Helmet>
+            <article className="android">
+                <header>
+                    <div className="text">
+                    <h1>{appName}</h1>
+                    <em>{appDesc}</em>
+                    </div>
+                </header>
+                <section>
+                    <div className="row">
+                        <div className="text">
+                            <header>
+
+                            </header>
+                        </div>
+                        <video>
+                            <source type="video/mp4" />
+                        </video>
+                    </div>
+                </section>
+
+            </article>
         </>
     );
 }
