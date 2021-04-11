d6ed06cc1ffaac40696fae2cf586512adacfa11c
thekauer <thekauer@gmail.com>
2021-04-11 16:41:18 +0200

    added rockefeller konami

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,13 +3,18 @@ import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import ScrollTop from './components/ScrollTop/ScrollTop';
 import Toolbar from './components/Toolbar/Toolbar';
+import './index.css'
 import './Layout.css'
 import useKonami from 'react-use-konami';
+import Popup from 'reactjs-popup';
+import moneyIcon from './dollar.svg';
+import Confetti from 'react-confetti'
 
 export default function Layout({children}:any) {
     const [checked,setChecked] = useState(false);
+    const [money,setMoney] = useState(false);
     useKonami(() => {
-        console.log('hi easter egg!');
+        setMoney(true);
     }, {
         code: ['r','o','c','k','e','f','e','l','l','e','r'], 
     });
@@ -17,6 +22,13 @@ export default function Layout({children}:any) {
             <>
             <BrowserRouter>
             <ScrollTop />
+            <Popup open={money} onClose={() => setMoney(false)}>
+                <div className="money">
+                    <Confetti className="confetti" recycle={false} initialVelocityY={20} numberOfPieces={5000}/>
+                    <h2>$10,000</h2>
+                    <img src={moneyIcon} alt="money" />
+                </div>
+            </Popup>
             <div className={(checked? "themelight" : "themedark") + " layout"}>
                 <Toolbar checked={checked} setChecked={setChecked}/>
                 <div className="content">
