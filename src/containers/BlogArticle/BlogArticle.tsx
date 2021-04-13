90888305dd2731ff1a1dea3a9f3c27a0d2910dcf
thekauer <thekauer@gmail.com>
2021-04-14 01:12:54 +0200

    migrated to styled-component

--- src/containers/BlogArticle/BlogArticle.tsx
+++ src/containers/BlogArticle/BlogArticle.tsx
@@ -1,9 +1,48 @@
 import { useEffect, useState } from 'react';
 import '../../index.css'
-import './BlogArticle.css'
 import formatDate from '../../util';
-import React from 'react';
 import {BlogItem} from '../Blog/Blog'
+import styled from 'styled-components';
+
+const StyledBlogArticle = styled.div`
+    height: 100%;
+    display:flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: var(--theme-dark);
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.5);   
+    border-radius: 5px;
+    border:solid 2px black; 
+    overflow: hidden;
+    transition: var(--transition-time);
+    margin: 2em 4em;
+`
+const Cover = styled.img`
+    width: 100%;
+    max-height: 400px;
+    object-fit: cover;
+    border-bottom: solid 2px black;
+`
+
+const Description = styled.div`
+    padding:1em;
+    padding-top: 0.5em;
+`
+const P = styled.p`
+    word-wrap: break-word;
+    max-width: 300px;
+    min-height: 4em;
+    padding: 0;
+`
+const Footer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    align-items: center;
+    padding:0 1em 1em 0.5em;
+    margin:0 0.5em;
+`
 
 const BlogArticle = (props : any) =>  {
 
@@ -16,20 +55,20 @@ const BlogArticle = (props : any) =>  {
         if(item!=null) {
             return (
             <>
-            <div className="blog-article">
-            <img src={item.image} alt="cover"></img>
-            <div className="blog-article-desc">
+            <StyledBlogArticle>
+            <Cover src={item.image} />
+            <Description>
             <h1>{item.title}</h1>
             <p>{item.desc}</p>
-            <p dangerouslySetInnerHTML={{__html: item.content}} />
+            <P dangerouslySetInnerHTML={{__html: item.content}} />
             <button onClick={() => {props.history.goBack();} }>go back {props.history.length}</button>
-            </div>
-            <div className="blog-article-footer">
-                <div className="date">
+            </Description>
+            <Footer>
+                <div>
                     {formatDate(item.date)}
                 </div>
-            </div>
-            </div>
+            </Footer>
+            </StyledBlogArticle>
 
             </>
             );
