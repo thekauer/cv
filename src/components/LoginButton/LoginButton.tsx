083b92965dbcb2bc49b8181336e21cc21b005fbe
thekauer <thekauer@gmail.com>
2021-04-15 01:03:42 +0200

    login button fix

--- src/components/LoginButton/LoginButton.tsx
+++ src/components/LoginButton/LoginButton.tsx
@@ -28,6 +28,21 @@ const UserIcon = styled.img`
         stroke: var(--active-font-color);
     }
 `
+const UserSvg = styled.svg`
+    max-height: 1em;
+    margin: 0 0.5em;
+    justify-self: flex-end;
+    border-radius: 100%;
+    transition: var(--transition-time);
+	stroke:var(--nav-font-color);
+	stroke-width:30;
+	fill:transparent;
+    cursor: pointer;
+    &:hover {
+        box-shadow: 0px 0px 5px black;
+        stroke: var(--active-font-color);
+    }
+`
 const LoginButton = () => {
     useEffect(() =>{
         auth.onAuthStateChanged((user) => {
@@ -65,11 +80,11 @@ const LoginButton = () => {
             {loggedInAsAdmin && <UserIcon src={settingsIcon}  alt="gear" onClick={toAdminPage}/>}
             <div className="profile" onClick={authUser}>
             {userProfile ? <UserIcon src={userProfile} alt="Profile"/> :
-            <svg width="16px" height="16px" fill="none" viewBox="0 0 600 600" strokeWidth="30px">
+            <UserSvg width="16px" height="16px" fill="none" viewBox="0 0 600 600" strokeWidth="30px">
                 <circle cx="300" cy="300" r="265" />
                 <circle cx="300" cy="230" r="115"/>
                 <path d="M106.81863443903,481.4 a205,205 1 0,1 386.36273112194,0" stroke-linecap="butt" />
-            </svg>
+            </UserSvg>
             }
             </div>
             </StyledLoginButton>
