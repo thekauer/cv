b151af48d3fb73ecb5ecf2bc6ff26ea689f25ae0
thekauer <thekauer@gmail.com>
2021-06-08 20:42:45 +0200

    added margin

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -7,6 +7,7 @@ interface CarouselProps {
 export const Carousel = styled.div<CarouselProps>`
     box-shadow: 0 0 32px black;
     border-radius:15px;
+    margin:1em;
 
     width:clamp(320px,100%,640px);
     height:clamp(240px,360px,480px);
