1859dbc11776b0135747232aa9c9fe247ee3287f
thekauer <thekauer@gmail.com>
2022-12-28 21:45:27 +0100

    feat: add i18n

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
-import Home from "./home";
+import Home from "../components/home";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-const Index = () =>(
-  <Home/>
-)
+export const getServerSideProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ["home", "common"])),
+  },
+});
+
+const Index = () => <Home />;
 
 export default Index;
