d13c5b3843bcf6820d66c7487a5deea9109ed536
thekauer <thekauer@gmail.com>
2022-12-29 21:14:03 +0100

    WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 import Home from "../components/home";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetStaticProps } from "next";
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
     ...(await serverSideTranslations(locale!, ["home", "common"])),
   },
 });
 
-const Index = () => <Home />;
+const Index = (props: any) => {
+  console.log(props);
+  return <Home />;
+};
 
 export default Index;
